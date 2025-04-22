233d368e322e187318b6724e0ebdf3b91a1c3359
haongnd2280 <hao.ngnd2285@gmail.com>
2025-04-23 00:48:18 +0700

    fix: fix some error when rename folder

--- src/sqlite_db/read_sqlite3_by_pandas.py
+++ src/sqlite_db/read_sqlite3_by_pandas.py
@@ -5,7 +5,7 @@ import pandas as pd
 conn = sqlite3.connect("example.db")
 
 # Read a table into a DataFrame
-df = pd.read_sql(sql="SELECT * FROM users", conn=conn)
+df = pd.read_sql(sql="SELECT * FROM users", con=conn)
 print(df)
 
 conn.close()
